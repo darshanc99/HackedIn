0d2a467c211574bfcc1b5065a18d1832a93a30d9
Darshan Chheda <ddc@somaiya.edu>
2019-08-16 20:00:03 +0530

    Update Profile Done

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
 	email = forms.EmailField()
@@ -13,3 +14,14 @@ class UserRegisterForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ['username','first_name','last_name','email','github_username','linkedin_URL','password1','password2']
+
+class UserUpdateForm(forms.ModelForm):
+	email = forms.EmailField()
+	class Meta:
+		model = User
+		fields = ['username','email']
+
+class ProfileUpdateForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		fields = ['image']

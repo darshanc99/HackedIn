69b98d097f6e607a0a8a401ee902ba4856a2ee61
Darshan Chheda <ddc@somaiya.edu>
2019-10-06 21:51:21 +0530

    Completed the Post Accomplishments

--- hub/urls.py
+++ hub/urls.py
@@ -2,8 +2,23 @@
 from django.urls import path
 from django.conf.urls import url,include
 from . import views
+from .views import (
+	PostListView,
+	PostDetailView,
+	PostCreateView,
+	PostUpdateView,
+	PostDeleteView
+	)
 
 urlpatterns = [
-	#/hackedin/
-	path('',views.network,name='network'),
+	#/community/
+	path('',PostListView.as_view(),name='network'),
+	#/community/<pk>
+	path('<int:pk>/',PostDetailView.as_view(),name='network-post'),
+	#/community/new/
+	path('new/',PostCreateView.as_view(),name='network-create'),
+	#/community/<pk>/update/
+	path('<int:pk>/update/',PostUpdateView.as_view(),name='network-update'),
+	#/community/<pk>/delete/
+	path('<int:pk>/delete/',PostDeleteView.as_view(),name='network-delete'),	
 ]
--- hub/views.py
+++ hub/views.py
@@ -1,9 +1,16 @@
 #Import Dependencies
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.http import HttpResponse
 from django.template import loader
 from .models import Accomplishment
+from django.views.generic import (ListView,
+	DetailView,
+	CreateView,
+	UpdateView,
+	DeleteView
+	)
 
 @login_required
 def network(request):
@@ -11,3 +18,47 @@ def network(request):
 	'accomplishments' : Accomplishment.objects.all()
 	}
 	return render(request,'hub/network.html',context)
+
+class PostListView(LoginRequiredMixin,ListView):
+	model = Accomplishment
+	template_name = 'hub/network.html' #<app>/<model>_<viewtype>.html
+	context_object_name = 'accomplishments'
+	ordering = ['-date_posted']
+
+class PostDetailView(LoginRequiredMixin,DetailView):
+	model = Accomplishment
+	template_name = 'hub/networkpost.html'
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+	model = Accomplishment
+	fields = ['title','content']
+	def form_valid(self,form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+	template_name = 'hub/post_form.html'
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+	model = Accomplishment
+	fields = ['title','content']
+	def form_valid(self,form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+	def test_func(self):
+		accomplishments = self.get_object()
+		if self.request.user == accomplishments.author:
+			return True
+		return False
+
+	template_name = 'hub/post_form.html'
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+	model = Accomplishment
+	success_url = '/community'
+	template_name = 'hub/networkpost.html'
+	def test_func(self):
+		accomplishments = self.get_object()
+		if self.request.user == accomplishments.author:
+			return True
+		return False
+	template_name = 'hub/post_confirm_delete.html'
